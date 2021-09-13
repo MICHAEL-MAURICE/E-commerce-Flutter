f8b9fcc144bd66cfd6e821d7eb3ec61d5ec15566
michael maurice <michaelmauricengn@gmail.com>
2021-09-13 22:03:33 +0200

    intilization project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,8 @@ import 'package:ecommerce_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+      bool? isDark;
+    await tester.pumpWidget(MyApp(isDark));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
