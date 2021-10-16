9fa2256c6bee4e2bbca052e03e9531ee854351ae
michael maurice <michaelmauricengn@gmail.com>
2021-10-17 01:47:09 +0200

    Onboarding

---
+++ lib/modules/onboarding/Login_Shop/Login_Shop.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Login_Shop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+        children: [
+          Text("LogIn",
+            style: TextStyle(fontSize: 18.0),)
+        ],
+      )
+    );
+  }
+}
